f23689e5ae4a5630099aba9ac30aef80d24bacc9
Martin Cochran <martin.cochran@gmail.com>
2016-06-12 21:14:50 -0700

    Update todos from last CL

--- index.ios.js
+++ index.ios.js
@@ -14,10 +14,9 @@ AppRegistry.registerComponent('AwesomeProject', () => MainComponent);
 
 /**
  * TODO:
- *  1. Use Reflux for MVC behavior.
+ *  1. Hook up division / league selection to state of GamesComponent and TouranmentComponent.
  *  2. Hide back button on home screen.
- *  3. Hook up division / league selection to state of GamesComponent and TouranmentComponent.
- *  4. Go directly to games for MLU & AUDL & grey out division selection.
- *  5. Async / local storage for game data.
- *  6. Determine sorting of club / college / mlu / audl by API query.
+ *  3. Go directly to games for MLU & AUDL & grey out division selection.
+ *  4. Async / local storage for game data.
+ *  5. Determine sorting of club / college / mlu / audl by API query.
  */
